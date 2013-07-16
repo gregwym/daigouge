86cdc42923dbcb727a31c99f48424a0e50d5ae7f
Greg Wang <gregwym@gmail.com>
2013-07-16 16:56:27 +0800

    Implement basic local auth process.

---
+++ app/controllers/auth.js
@@ -0,0 +1,39 @@
+var localPassport = require('../passport/local');
+
+exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/auth/local');
+};
+
+exports.local = function(req, res) {
+  res.render('auth/local', {
+    user: req.user,
+    error: req.flash('error'),
+    success: req.flash('success')
+  });
+};
+
+exports.local.register = function(req, res) {
+  var email = req.body.email,
+      pass = req.body.password;
+
+  // Both field are required
+  if(!email || !pass) {
+    req.flash('error', 'Please fill both Email and Password');
+    return res.redirect('/auth/local');
+  }
+
+  // Try create a new user, flash 'error' message if failed.
+  localPassport.createNewUser(email, pass, function(err, user) {
+    if(err) {
+      req.flash('error', 'Fail to register - ' + err);
+      return res.redirect('/auth/local');
+    } else {
+      req.login(user, function(err) {
+        if(err) { req.flash('error', 'Fail to login - ' + err); }
+        else { req.flash('success', 'Account ' + email + ' created'); }
+        return res.redirect('/auth/local');
+      });
+    }
+  });
+};
