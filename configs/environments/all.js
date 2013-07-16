d98bce91d84a94ee925c8453f77678209e0f1d36
Greg Wang <gregwym@gmail.com>
2013-07-16 12:55:26 +0800

    Add mongo support and use as session storage.

--- configs/environments/all.js
+++ configs/environments/all.js
@@ -1,15 +1,24 @@
-var express = require('express');
+var express = require('express'),
+    MongoStore = require('connect-mongo')(express),
+    mongoose = require('mongoose');
 
 module.exports = function(app, basedir) {
   app.set('port', process.env.PORT || 3000);
   app.set('views', basedir + '/app/views');
   app.set('view engine', 'jade');
+
+  // Config middleware invocation sequence
   app.use(express.favicon());
+  app.use(express.static(basedir + '/public'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  app.use(express.session({
+    secret: 'your secret here',
+    store: new MongoStore({
+      db: mongoose.connection.db
+    })
+  }));
   app.use(app.router);
-  app.use(express.static(basedir + '/public'));
 };
