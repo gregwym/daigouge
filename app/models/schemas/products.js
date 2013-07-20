7005f3a419da6556064e94f7668a8ef462e2998d
Greg Wang <gregwym@gmail.com>
2013-07-20 17:58:11 +0800

    Validate url format in products schema.

--- app/models/schemas/products.js
+++ app/models/schemas/products.js
@@ -9,11 +9,15 @@ var currencies = 'CAD$ US$ RMB¥'.split(' ');
 
 var products = mongoose.Schema({
   name: { type: String, required: true },   // Product name
-  date: {
-    c: { type: Date, default: Date.now },   // Create date
-    lm: { type: Date, default: Date.now }   // Last modified date
+  date: {                                   // Associated dates
+    c: { type: Date, default: Date.now },     // Created date
+    lm: { type: Date, default: Date.now }     // Last modified date
   },
-  url: { type: String, required: true },    // Product source URL
+  url: {                                    // Product source URL
+    type: String,
+    match: /^https{0,1}:\/\/.+/,
+    required: true
+  },
   prices: [{                                // Associated prices
     value: { type: Number, required: true },  // Price value
     type: {                                   // Price type
