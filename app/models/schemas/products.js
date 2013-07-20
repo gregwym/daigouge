0cbd2a7688de64cc45ce1e468a6dade29d80b228
Greg Wang <gregwym@gmail.com>
2013-07-20 16:30:02 +0800

    Define products model

---
+++ app/models/schemas/products.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+// Price types:
+//  - reg: Regular Price
+//  - sale: On sale price
+//  - org: Original price fetched from the URL
+var priceTypes = 'reg sale org'.split(' ');
+var currencies = 'CAD$ US$ RMB¥'.split(' ');
+
+var products = mongoose.Schema({
+  name: { type: String, required: true },   // Product name
+  date: {
+    c: { type: Date, default: Date.now },   // Create date
+    lm: { type: Date, default: Date.now }   // Last modified date
+  },
+  url: { type: String, required: true },    // Product source URL
+  prices: [{                                // Associated prices
+    value: { type: Number, required: true },  // Price value
+    type: {                                   // Price type
+      type: String,
+      enum: priceTypes,
+      default: priceTypes[0],
+      required: true
+    },
+    cur: {                                    // Price currency (optional)
+      type: String,
+      enum: currencies
+    }
+  }]
+});
+
+module.exports = products;
