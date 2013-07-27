66416c4a037b1eb595ea9609919c3c39a2e7cce7
Greg Wang <gregwym@gmail.com>
2013-07-27 14:21:14 +0800

    Create nav-view component.

---
+++ client/nav-view/index.js
@@ -0,0 +1,30 @@
+
+/**
+ * Module dependencies.
+ */
+
+var domify = require('domify'),
+    html = require('./template'),
+    NavItemView = require('nav-item-view');
+
+/**
+ * Expose `NavVIew`.
+ */
+
+module.exports = NavView;
+
+/**
+ * Initialize a new view for `items`.
+ *
+ * @param {Items} items
+ * @api public
+ */
+
+function NavView(items) {
+  var navList = this.el = domify(html);
+  for (var i = 0; i < items.length; i++) {
+    var itemView = new NavItemView(items[i]);
+    navList.appendChild(itemView.el);
+    console.log('Add nav item ' + items[i]);
+  }
+}
