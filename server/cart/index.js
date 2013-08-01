ca529b51fb1a5b204f30aced68ed1eb02bc8f1e1
Greg Wang <gregwym@gmail.com>
2013-08-01 22:40:57 +0800

    Use quantity instead of amount and fix bugs.

--- server/cart/index.js
+++ server/cart/index.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     models = require('models');
 var app = module.exports = express();
+var debug = require('debug')('cart');
 
 // Settings
 app.set('views', __dirname);
 
-var itemField = 'product amount'.split(' ');
+var itemField = 'product quantity'.split(' ');
 var validItem = function(item) {
   if (item === null) { return false; }
   for (var i = 0; i < itemField.length; i++) {
@@ -19,49 +20,32 @@ var prepareCart = function(req, res, next) {
   next();
 };
 
-var format = function(req, res, html) {
-  return function(err, result) {
-    if (err) { return res.status(500).json(err); }
-    return res.format({
-      html: function() { html(result); },
-      json: function() { res.json(result); }
-    });
-  };
-};
-
-app.get('*', prepareCart);
+app.all('*', prepareCart);
 
 // List all
 app.get('/', function(req, res) {
   res.format({
-    html: function() { res.send('list all in cart'); },
+    html: function() { res.send(req.session.cart); },
     json: function() { res.json(req.session.cart); }
   });
 });
 
-// Add new
+// Add new or Update existing
 app.post('/', function(req, res) {
   var item = req.body;
   if (!validItem(item)) {
     return res.status(400);
   }
+  debug('Adding ' + JSON.stringify(item) + ' to cart.');
   req.session.cart[item.product] = item;
   res.status(201).json(req.session.cart);
 });
 
-// Update existing
-app.put('/:product', function(req, res){
-  var item = req.body;
-  if (!validItem(item) || item.product != req.params.product) {
-    return res.status(400);
-  }
-  req.session.cart[req.params.product] = item;
-  res.json(req.session.cart);
-});
-
 // Remove item
 app.delete('/:product', function(req, res) {
-  if (req.session.cart[req.params.product] !== null) {
+  var item = req.session.cart[req.params.product];
+  if (item) {
+    debug('Deleting ' + JSON.stringify(item) + ' from cart.');
     delete req.session.cart[req.params.product];
   }
   res.json(req.session.cart);
