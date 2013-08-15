067f3faf14b51a1705a5d96fd36c72b8ff2df74f
Greg Wang <gregwym@gmail.com>
2013-08-15 16:13:08 +0800

    Add user populator.

--- server/utils/populators.js
+++ server/utils/populators.js
@@ -1,7 +1,14 @@
 var models = require('models');
 
-exports.products = function(orgItems, next) {
-  models.products.populate(orgItems, {
+exports.products = function(docs, next) {
+  models.products.populate(docs, {
     path: 'prod'
+  }, next);
+};
+
+exports.users = function(docs, next) {
+  models.users.populate(docs, {
+    path: 'user',
+    select: 'email reg'
   }, next);
 };
