51f5c50b72488171d0f50a9c5d74f1cad2d2cd58
Greg Wang <gregwym@gmail.com>
2013-08-17 17:53:53 +0800

    Check DEBUG env first.

--- server/utils/top.js
+++ server/utils/top.js
@@ -3,5 +3,5 @@ var debug = require('debug')('taobao');
 exports.topClient = require('top.js').createClient({
   app_key: process.env.TOP_APP_KEY,
   app_secret: process.env.TOP_APP_SECRET,
-  debug: process.env.DEBUG.indexOf('taobao') != -1
+  debug: process.env.DEBUG && (process.env.DEBUG.indexOf('taobao') != -1)
 });
