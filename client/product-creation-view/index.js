922439c8c94024498956c55dae0076b32ad6e0bf
Greg Wang <gregwym@gmail.com>
2013-08-22 19:31:21 +0800

    Create product then display it with product-view component.

--- client/product-creation-view/index.js
+++ client/product-creation-view/index.js
@@ -2,14 +2,14 @@ var html = require('./template'),
     domify = require('domify'),
     query = require('query'),
     value = require('value'),
-    PreviewView = require('./preview-view'),
+    request = require('superagent'),
+    ProductView = require('product-view'),
     View = require('view');
 
 module.exports = ProductCreationView;
 
 function ProductCreationView(strategy) {
-  var els = domify(html);
-  var el = query('.product-creation', els);
+  var el = domify(html);
   View.call(this, {
     urlPlaceholder: 'URL',
     fetchText: 'Go'
@@ -22,13 +22,16 @@ function ProductCreationView(strategy) {
 ProductCreationView.prototype.fetch = function() {
   var self = this;
   var url = value(self.urlText);
-  self.strategy.fetch(url, function(err, item) {
-    var previewView = new PreviewView(item, self.strategy);
-    var oldEl = query('.product-preview', self.el);
-    if (oldEl) {
-      self.el.replaceChild(previewView.el, oldEl);
-    } else {
-      self.el.appendChild(previewView.el);
-    }
-  });
+  var post = self.strategy.postData(url);
+  request
+    .post('/products/' + self.strategy.name)
+    .set('Accept', 'application/json')
+    .send(post)
+    .end(function(err, res) {
+      if (err) { return alert(JSON.stringify(err)); }
+
+      var productView = new ProductView(res.body);
+      var el = query('.product-view', self.el);
+      self.el.replaceChild(productView.el, el);
+    });
 };
--- client/product-creation-view/taobao.js
+++ client/product-creation-view/taobao.js
@@ -1,28 +1,15 @@
 var request = require('superagent');
 
-exports.fetch = function(url, next) {
+exports.name = 'taobao';
+
+exports.postData = function(url) {
   var matchs = url.match(/id=(\w+)/);
   if (matchs.length < 2) {
-    return next(new Error('No matching data found'));
+    throw new Error('No matching id found');
   }
-  var query = {
+  var post = {
     id: matchs[1]
   };
 
-  request.get('/products/new/taobao').query(query).end(function(err, res) {
-    next(err, res.body.item);
-  });
-};
-
-exports.methods = {
-  priceTag: function() {
-    if (this.item.skus && this.item.skus.sku && this.item.skus.sku.length) {
-      return this.item.skus.sku.map(function(sku) { return sku.price; });
-    }
-    return this.item.price;
-  },
-  shipping: function() {
-    if (this.item.freight_payer == 'seller') { return 0; }
-    return this.item.post_fee;
-  }
+  return post;
 };
