cb3f4b398caa8ae182ee8b9a3ce85db093177509
Greg Wang <gregwym@gmail.com>
2013-08-24 15:24:38 +0800

    Use strings-choosable for product properties selection.

--- client/product-view/index.js
+++ client/product-view/index.js
@@ -3,17 +3,53 @@ var html = require('./template'),
     query = require('query'),
     request = require('superagent'),
     AddToCart = require('product-add-to-cart'),
+    StringsChoosable = require('strings-choosable'),
     View = require('view');
 
 module.exports = ProductView;
 
 function ProductView(product) {
   this.product = product;
+  this.propsSelection = {};
 
+  // Construct view
+  var self = this;
   var el = domify(html);
   View.call(this, product, el);
+  this.priceEl = query('.product-price', this.el);
 
+  // Query product-actions element
   var actionsEl = query('.product-actions', this.el);
+
+  // Construct property selections
+  var properties = product.properties;
+  if (properties) {
+
+    // On selection change handler maker
+    var makeOnSelectionChange = function(prop) {
+      return function(selected, el) {
+        self.updatePropertySelection(prop, selected);
+      };
+    };
+
+    // Construct selection component for each property
+    for (var prop in properties) {
+      this.propsSelection[prop] = null;
+
+      // Get all the property values
+      var values = [];
+      for (var value in properties[prop]) {
+        values.push(value);
+      }
+
+      // Construct strings-choosable component
+      var propsChoosable = new StringsChoosable(values);
+      propsChoosable.on('value-change', makeOnSelectionChange(prop));
+      actionsEl.appendChild(propsChoosable.el);
+    }
+  }
+
+  // Construct add-to-cart component
   var addToCart = new AddToCart(this);
   actionsEl.appendChild(addToCart.el);
 }
@@ -39,4 +75,14 @@ ProductView.prototype.getAddToCartItem = function() {
   return {
     prod: this.product.id,
   };
+};
+
+ProductView.prototype.updatePropertySelection = function(property, value) {
+  this.propsSelection[property] = value;
+  console.log('Properties selection: ' + JSON.stringify(this.propsSelection));
+  var sku = this.product.findSkuByProps(this.propsSelection);
+  if (sku === null) { return; }
+
+  console.log('Found matched sku: ' + JSON.stringify(sku));
+  this.priceEl.textContent = sku.price;
 };
